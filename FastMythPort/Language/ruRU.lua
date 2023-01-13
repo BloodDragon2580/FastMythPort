7f21e6bcbb51564c946958da4745ea26bd6453b9
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2023-01-13 17:11:39 +0500

    Update ruRU.lua

--- FastMythPort/Language/ruRU.lua
+++ FastMythPort/Language/ruRU.lua
@@ -4,34 +4,34 @@ local L = me.L
 
 L["|cffd6266cFastMythPort|r"] = "|cffd6266cFastMythPort|r"
 
-L["Keystone Hero Portals"] = "Keystone Hero"
-L["Fate of Mythic Raids"] = "Fate of Mythic"
+L["Keystone Hero Portals"] = "Порталы 'Герой ключей'"
+L["Fate of Mythic Raids"] = "Судьбоносные М+ рейды"
 
 --Tabs
-L["Available"] = "Available"
-L["Unobtained"] = "Unobtained"
+L["Available"] = "Доступные"
+L["Unobtained"] = "Не получено"
 
 --Minimap Overlay
-L["Click to toggle main ui"] = "Click to toggle main ui"
-L["Shift + Click to toggle favorites"] = "Shift + Click to toggle favorites"
+L["Click to toggle main ui"] = "[ЛКМ] - открыть окно FastMythPort"
+L["Shift + Click to toggle favorites"] = "[Shift+ПКМ] - открыть окно 'Избранные порталы'"
 
 --Chat Commands
-L["List of slash commands:"] = "List of slash commands:"
-L["|cff00cc66/fmp|r - toggle ui"] = "|cff00cc66/fmp|r - toggle ui"
-L["|cff00cc66/fmp favorites|r - toggles favorites ui"] = "|cff00cc66/fmp favorites|r - toggles favorites ui"
-L["|cff00cc66/fmp refresh|r - refreshes collection"] = "|cff00cc66/fmp refresh|r - refreshes collection"
-L["|cff00cc66/fmp help|r - shows help info"] = "|cff00cc66/fmp help|r - shows help info"
-L["|cff00cc66/fmp debug|r - toggle debug mode"] = "|cff00cc66/fmp debug|r - toggle debug mode"
-L["Debug mode on!"] = "Debug mode on!"
-L["Debug mode off!"] = "Debug mode off!"
-L["Unknown command!"] = "Unknown command!"
+L["List of slash commands:"] = "Список слэш-команд:"
+L["|cff00cc66/fmp|r - toggle ui"] = "|cff00cc66/fmp|r - открыть окно FastMythPort"
+L["|cff00cc66/fmp favorites|r - toggles favorites ui"] = "|cff00cc66/fmp favorites|r - открыть окно 'Избранные порталы'"
+L["|cff00cc66/fmp refresh|r - refreshes collection"] = "|cff00cc66/fmp refresh|r - обновить коллекцию порталов"
+L["|cff00cc66/fmp help|r - shows help info"] = "|cff00cc66/fmp help|r - показать справочную информацию"
+L["|cff00cc66/fmp debug|r - toggle debug mode"] = "|cff00cc66/fmp debug|r - вкл./выкл. режим отладки"
+L["Debug mode on!"] = "Режим отладки включен!"
+L["Debug mode off!"] = "Режим отладки выключен!"
+L["Unknown command!"] = "Неизвестная команда!"
 
 --Buttons
-L["You can not have more than "] = "You can not have more than "
-L[" favorites!"] = " favorites!"
-L["refresh"] = "refresh"
+L["You can not have more than "] = "Вы не можете иметь больше, чем "
+L[" favorites!"] = " избранное!"
+L["refresh"] = "обновить"
 
 --Favoriten
-L["Hold Left-Click to drag"] = "Hold Left-Click to drag"
-L["Right-Click to toggle"] = "Right-Click to toggle"
-L["Selected Favorites: "] = "Selected Favorites: "
+L["Hold Left-Click to drag"] = "[Удерживать ЛКМ] - чтобы перетащить"
+L["Right-Click to toggle"] = "[ПКМ] - чтобы переключить"
+L["Selected Favorites: "] = "Избранное: "
