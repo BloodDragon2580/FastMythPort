5c05f4c8c9f91d191b0f196c19db3ba74ca15362
BloodDragon2580 <gewinner.thomas@gmail.com>
2023-01-23 02:15:11 +0100

    Update to FastMythPort.v.2.3

--- FastMythPort/FastMythPort.lua
+++ FastMythPort/FastMythPort.lua
@@ -9,7 +9,21 @@ core.sourceOrder = {
 }
 
 FastMythPort[L["Keystone Hero Portals"]] = {
-    -- 9.2 SL Season 3
+    {id = 131229, type = 'spell', name = 'Path of the Scarlet Mitre', unobtainable = false},
+    {id = 159900, type = 'spell', name = 'Path of the Dark Rail', unobtainable = false},
+    {id = 131205, type = 'spell', name = 'Path of the Stout Brew', unobtainable = false},
+    {id = 131206, type = 'spell', name = 'Path of the Shado-Pan', unobtainable = false},
+    {id = 131222, type = 'spell', name = 'Path of the Mogu King', unobtainable = false},
+    {id = 131225, type = 'spell', name = 'Path of the Setting Sun', unobtainable = false},
+    {id = 131232, type = 'spell', name = 'Path of the Necromancer', unobtainable = false},
+    {id = 159902, type = 'spell', name = 'Path of the Burning Mountain', unobtainable = false},
+    {id = 159898, type = 'spell', name = 'Path of the Skies', unobtainable = false},
+    {id = 131228, type = 'spell', name = 'Path of the Black O', unobtainable = false},
+    {id = 159895, type = 'spell', name = 'Path of the Bloodmaul', unobtainable = false},
+    {id = 159897, type = 'spell', name = 'Path of the Vigilant', unobtainable = false},
+    {id = 131231, type = 'spell', name = 'Path of the Scarlet Blade', unobtainable = false},
+    {id = 159901, type = 'spell', name = 'Path of the Verdant', unobtainable = false},
+--
     {id = 354462, type = 'spell', name = 'Path of the Courageous', unobtainable = false},
     {id = 354463, type = 'spell', name = 'Path of the Plagued', unobtainable = false},
     {id = 354464, type = 'spell', name = 'Path of the Misty Forest', unobtainable = false},
@@ -19,12 +33,10 @@ FastMythPort[L["Keystone Hero Portals"]] = {
     {id = 354468, type = 'spell', name = 'Path of the Scheming Loa', unobtainable = false},
     {id = 354469, type = 'spell', name = 'Path of the Stone Warden', unobtainable = false},
     {id = 367416, type = 'spell', name = 'Path of the Streetwise Merchant', unobtainable = false},
-    -- 9.2.5 SL Season 4,
     {id = 159896, type = 'spell', name = 'Path of the Iron Prow', unobtainable = false},
     {id = 159900, type = 'spell', name = 'Path of the Dark Rail', unobtainable = false},
     {id = 373262, type = 'spell', name = 'Path of the Fallen Guardian', unobtainable = false},
     {id = 373274, type = 'spell', name = 'Path of the Scrappy Prince', unobtainable = false},
-    -- 10.0.2 DF Season 1,
     {id = 159899, type = 'spell', name = 'Path of the Crescent Moon', unobtainable = false},
     {id = 131204, type = 'spell', name = 'Path of the Jade Serpent', unobtainable = false},
     {id = 393766, type = 'spell', name = 'Path of the Grand Magistrix', unobtainable = false},
