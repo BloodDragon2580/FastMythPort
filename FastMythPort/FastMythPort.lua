102d6001675f58c229a4eb9885fbe00692424803
BloodDragon2580 <gewinner.thomas@gmail.com>
2025-03-17 06:46:12 +0100

    'Update'

--- FastMythPort/FastMythPort.lua
+++ FastMythPort/FastMythPort.lua
@@ -27,7 +27,7 @@ FastMythPort[L["Mists of Pandaria"]] = {
     {id = 131206, type = 'spell', name = 'Path of the Shado-Pan', unobtainable = false},
     {id = 131222, type = 'spell', name = 'Path of the Mogu King', unobtainable = false},
     {id = 131225, type = 'spell', name = 'Path of the Setting Sun', unobtainable = false},
-    {id = 131228, type = 'spell', name = 'Path of the Black O', unobtainable = false},
+    {id = 131228, type = 'spell', name = 'Path of the Black Ox', unobtainable = false},
     {id = 131205, type = 'spell', name = 'Path of the Stout Brew', unobtainable = false},
     {id = 131232, type = 'spell', name = 'Path of the Necromancer', unobtainable = false},
     {id = 131231, type = 'spell', name = 'Path of the Scarlet Blade', unobtainable = false},
@@ -67,22 +67,18 @@ FastMythPort[L["Shadowlands"]] = {
     {id = 354464, type = 'spell', name = 'Path of the Misty Forest', unobtainable = false},
     {id = 354467, type = 'spell', name = 'Path of the Undefeated', unobtainable = false},
     {id = 354465, type = 'spell', name = 'Path of the Sinful Soul', unobtainable = false},
-    {id = 373262, type = 'spell', name = 'Path of the Fallen Guardian', unobtainable = false},
     {id = 354466, type = 'spell', name = 'Path of the Ascendant', unobtainable = false},
     {id = 354468, type = 'spell', name = 'Path of the Scheming Loa', unobtainable = false},
     {id = 354469, type = 'spell', name = 'Path of the Stone Warden', unobtainable = false},
     {id = 354462, type = 'spell', name = 'Path of the Courageous', unobtainable = false},
     {id = 354463, type = 'spell', name = 'Path of the Plagued', unobtainable = false},
-    {id = 373274, type = 'spell', name = 'Path of the Scrappy Prince', unobtainable = false},
     {id = 367416, type = 'spell', name = 'Path of the Streetwise Merchant', unobtainable = false}
 }
 
 FastMythPort[L["Dragonflight"]] = {
-    {id = 393766, type = 'spell', name = 'Path of the Grand Magistrix', unobtainable = false},
     {id = 393279, type = 'spell', name = 'Path of Arcane Secrets', unobtainable = false},
     {id = 393256, type = 'spell', name = 'Path of the Clutch Defender', unobtainable = false},
     {id = 393273, type = 'spell', name = 'Path of the Draconic Diploma', unobtainable = false},
-    {id = 393764, type = 'spell', name = 'Path of Proven Worth', unobtainable = false},
     {id = 393262, type = 'spell', name = 'Path of the Windswept Plains', unobtainable = false},
     {id = 393283, type = 'spell', name = 'Path of the Titanic Reservoir', unobtainable = false},
     {id = 393222, type = 'spell', name = 'Path of the Watchers Legacy', unobtainable = false},
@@ -119,5 +115,6 @@ FastMythPort[L["Dragonflight Raids"]] = {
 }
 
 FastMythPort[L["War Within Raids"]] = {
-    {id = 1226482, type = 'spell', name = 'Liberation of Undermine', unobtainable = false}
+    {id = 1226482, type = 'spell', name = 'Path of the Liberation of Undermine', unobtainable = false},
+    {id = 1226483, type = 'spell', name = 'Path of the Nerub\'ar Palace', unobtainable = false}
 }
